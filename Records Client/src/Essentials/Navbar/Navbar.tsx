c26326931a87432bb29e162935cce2767f946aad
Muaaz <yourssharky@gmail.com>
2024-01-31 22:06:12 +0500

    update 5 files and delete 1 file

--- Records Client/src/Essentials/Navbar/Navbar.tsx
+++ Records Client/src/Essentials/Navbar/Navbar.tsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   return (
     <nav className="w-screen z-50 md:h-[4rem] max-md:h-[5rem] bg-[var(--bg)] md:px-8 max-md:px-4 border-b border-gray-300 flex justify-between">
         
-      <section className="flex   w-1/2 p-2 h-full items-center object-fit">
-    <img src="/images/Records2.png" className="aspect-square rounded h-full cursor-pointer" alt="Records" onClick={()=>navigate("/")}/>
+      <section className="flex   w-1/2 p-2 h-full items-center object-contain">
+    <img src="/images/Records2.png" className="aspect-square object-contain rounded  cursor-pointer h-full" alt="Records" onClick={()=>navigate("/")}/>
     {data.isLogined?
     <LoginedSearchbar/>:
     <h1 className="text-3xl  BFont cursor-pointer " onClick={()=>navigate("/")}>Records</h1>
--- Records Client/src/main.tsx
+++ Records Client/src/main.tsx
@@ -3,9 +3,12 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { Store } from './app/ReduxStore.ts'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={Store}>
 <App />
   </Provider>
 )
+serviceWorkerRegistration.unregister()
+
