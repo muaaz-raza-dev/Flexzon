bf4e3327b801a4921fd5c72d5038781a7732cf6c
Muaaz <yourssharky@gmail.com>
2024-02-01 23:04:44 +0500

    update Polling.tsx

--- Records Client/src/Pages/Write/Poll&Q/Polling.tsx
+++ Records Client/src/Pages/Write/Poll&Q/Polling.tsx
@@ -1,5 +1,4 @@
-import { useAppDispatch, useAppSelector } from "@/app/ReduxHooks";
-import { WriteInsertion } from "@/app/Slices/WriteSlice";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
