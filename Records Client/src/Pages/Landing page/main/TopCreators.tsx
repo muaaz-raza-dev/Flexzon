fe99cc18b3f93baf8dd5f7c3a35630406843cdf9
Muaaz <yourssharky@gmail.com>
2024-02-15 22:20:09 +0500

    update TopCreators.tsx

--- Records Client/src/Pages/Landing page/main/TopCreators.tsx
+++ Records Client/src/Pages/Landing page/main/TopCreators.tsx
@@ -1,7 +1,6 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Button } from "@/components/ui/button";
 import { useAppSelector } from "@/app/ReduxHooks";
 import { FC } from "react";
 import { ItopCreators } from "@/app/Types/Ilanding";
