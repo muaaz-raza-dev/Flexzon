8f5b627af58cb0475c75c403fb7de4f4dd047e60
Muaaz <yourssharky@gmail.com>
2024-02-28 22:32:59 +0500

    delete 1 file and update 2 files

--- Records Client/src/Pages/Landing page/header/lpHeaderFile.tsx
+++ Records Client/src/Pages/Landing page/header/lpHeaderFile.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react"
-import {Home} from "lucide-react"
+import { FC,  MutableRefObject, useEffect, useRef, useState } from "react"
+import {ChevronRight,} from "lucide-react"
 import { useAppDispatch, useAppSelector } from "@/app/ReduxHooks"
 import { insertion } from "@/app/Slices/LandingSlice"
 import FetchBlogs from "@/Queryfunctions/Hooks/useFetchBlogs"
@@ -9,7 +9,7 @@ export const HeaderLabels:FC<{value:string}> = ({value}) => {
   let Credits=useAppSelector(state=>state.credits)
   let dispatch=useAppDispatch()
 return (
-      <div  className={` z-40 mt-1 ${ data.selectedTabs===value&&'!text-white !bg-[var(--primary)]'} flex items-center text-sm h-full bg-gray-200 p-2 rounded-lg text-gray-600 text-nowrap cursor-pointer hover:text-[var(--primary)] transition-colors `} onClick={()=>{
+      <div  className={`  mt-1 ${ data.selectedTabs===value&&'!text-white !bg-[var(--primary)]'} flex items-center text-sm h-full bg-gray-200 p-2 px-3 rounded-lg text-gray-600 text-nowrap cursor-pointer hover:text-[var(--primary)] transition-colors `} onClick={()=>{
         dispatch(insertion({selectedTabs:value,Blogs:[],count:0}))
           FetchBlogs<typeof dispatch>({...data,selectedTabs:value,Blogs:[],count:0},dispatch,Credits)
   }}>
@@ -20,17 +20,55 @@ return (
 
 export const LpHeaderFile = () => {
   let data=useAppSelector(state=>state.landing)
+  let ContainerRef:MutableRefObject<HTMLElement|null> = useRef<HTMLElement>(null)
+  const [RightorLeft, setRightorLeft] = useState<"Right"|"Left"|"none"|"both">("none");
+
+  useEffect(() => {
+HandleSlide(0)
+  }, []);
+  let HandleSlide = (offset:number)=>{
+    let containerscrollL = ContainerRef.current?.scrollLeft??0
+    let Containerwidth  = ContainerRef.current?.clientWidth??0
+    let containerscrollR = -(ContainerRef.current?.scrollLeft??0)+(ContainerRef.current?.scrollWidth??0) - Containerwidth
+
+
+    if ( ContainerRef.current) {
+      ContainerRef.current.scrollLeft += offset
+    }
+    if (containerscrollL+offset<50&&containerscrollR-offset<50) {
+    setRightorLeft("none")  
+    }
+    
+    else if (containerscrollR-offset>50&&containerscrollL+offset>50) setRightorLeft("both")
+    else if (containerscrollR-offset<50) setRightorLeft("Left")
+    else if (containerscrollL+offset<50) setRightorLeft("Right")
+console.log(containerscrollL+offset,containerscrollR+offset+"R",Containerwidth);
+
+  }
   return (
-    <section className="overflow-x-auto overflow-y-hidden pt-3  px-4 py-2 flex gap-x-4 
-    bg-[var(--bg)] z-[30]   w-screen Headerlp  border-b items-center sticky top-0 h-14 " >
-        <a href="#" className="p-1   rounded cursor-pointer 
-        hover:text-[var(--primary)]">
-<Home className="max-md:w-6 md:w-6 " size={28} />
-        </a>
+    <div className="flex w-screen center  sticky top-0 h-14   bg-[var(--bg)] z-[30] ">
+    
+       
+  
+   <div className={` px-4 bg-white z-10 absolute left-2 w-8 center  ${   ( RightorLeft=="Left"||RightorLeft=="both")?" visible":"invisible"}`}>
+        <button onClick={()=>HandleSlide(-200)} className=" rounded bg-[var(--primary)] text-white hover:bg-slate-600 z-20  aspect-square HeaderLShadow   p-2 "><ChevronRight size={16}/></button>
+      </div>
+    <section className=" px-1 overflow-x-auto relative  overflow-y-hidden pt-3   py-2 flex gap-x-4 
+    bg-[var(--bg)]  scroll-smooth  w-screen Headerlp  border-b items-center   " ref={ContainerRef}>
+  
+
+
         {
-   data.tabs.slice(0,15).map((elm)=>{  
-            return <HeaderLabels  value={elm.title} />
-        })}
-    </section>
+          data.tabs.slice(0,15).map((elm)=>{  
+            return <HeaderLabels   value={elm.title} />
+          })}
+
+      </section>
+<div className={` px-4 relative  right-2 w-8 z-20 center  ${   ( RightorLeft=="Right"||RightorLeft=="both")?" visible":"invisible"}`}>
+        <button onClick={()=>HandleSlide(200)} className=" rounded bg-[var(--primary)] text-white hover:bg-slate-600  aspect-square HeaderLShadow   p-2 "><ChevronRight size={16}/></button>
+      </div>
+     
+        
+          </div>
   )
 }
